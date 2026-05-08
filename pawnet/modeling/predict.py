61dd0459d990123cfec9074f66a82c380e2f5705
dsuzener <141687334+dsuzener@users.noreply.github.com>
2026-05-08 04:20:25 +0200

    Update predict.py

--- pawnet/modeling/predict.py
+++ pawnet/modeling/predict.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from typing import cast
 from loguru import logger
 from tqdm import tqdm
 import typer
@@ -7,6 +7,8 @@ import typer
 from pawnet.config import MODELS_DIR, PROCESSED_DATA_DIR
 
 import torch
+import torch.nn as nn
+
 from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
 from PIL import Image
 
@@ -15,38 +17,36 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
+    val_loader,
     features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
     model_path: Path = MODELS_DIR / "model.pkl",
     predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
-    # -----------------------------------------
 ):
-    # # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    # logger.info("Performing inference for model...")
-    # for i in tqdm(range(10), total=10):
-    #     if i == 5:
-    #         logger.info("Something happened for iteration 5.")
-    # logger.success("Inference complete.")
-    # # -----------------------------------------
+    model = efficientnet_b0()
+    model.classifier[1] = nn.Linear(
+        cast(nn.Linear, model.classifier[1]).in_features,
+        2
+    )
+    model.load_state_dict(torch.load(model_path, weights_only=True))
     weights = EfficientNet_B0_Weights.DEFAULT
-    model = efficientnet_b0(weights=weights)
     model.eval()
 
-    preprocess = weights.transforms()
 
-    img = Image.open("data/raw/oxford-iiit-pet/images/Siamese_71.jpg").convert("RGB")
-    input_tensor = preprocess(img).unsqueeze(0)
+    for images, labels in tqdm(val_loader, desc="Predicting"):
+        with torch.no_grad():
+            output = model(images)
 
-    with torch.no_grad():
-        output = model(input_tensor)
+        probs = torch.nn.functional.softmax(output[0], dim=0)
 
-    probs = torch.nn.functional.softmax(output[0], dim=0)
+        class_id = int(probs.argmax().item())
+        label = weights.meta["categories"][class_id]
+        if label == 'tench':
+            label = 'cat'
+        else:
+            label = 'dog'
+        score = probs[class_id].item()
 
-    class_id = int(probs.argmax().item())
-    label = weights.meta["categories"][class_id]
-    score = probs[class_id].item()
-
-    print(label, score)
+        print(label, score)
 
 
 if __name__ == "__main__":
