fa40bcb2735d9fd84aef37324436079ccbd50c96
dsuzener <141687334+dsuzener@users.noreply.github.com>
2026-05-08 04:20:27 +0200

    Update main.py

--- pawnet/main.py
+++ pawnet/main.py
@@ -1,10 +1,15 @@
 from pawnet.dataset import main as dataset
-
+from pawnet.modeling.train import main as train
+from pawnet.modeling.predict import main as predict
 
 def main():
     print("Hello from dd2424-group-project!")
 
-    train, val = dataset()
+    train_loader, val_loader = dataset()
+
+    # train(train_loader=train_loader)
+    predict(val_loader=val_loader)
+
 
 
 if __name__ == "__main__":
