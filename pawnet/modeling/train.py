2c0f2ffd6abc383acd760fc90d6ad318d340a70d
dsuzener <141687334+dsuzener@users.noreply.github.com>
2026-05-13 13:13:06 +0200

    Update train.py

--- pawnet/modeling/train.py
+++ pawnet/modeling/train.py
@@ -11,6 +11,8 @@ import torch.optim as optim
 import torchvision.models as models
 import torch.nn as nn
 
+from pawnet.utils import get_model
+
 app = typer.Typer()
 
 
@@ -22,47 +24,39 @@ def main(
     features_path: Path = PROCESSED_DATA_DIR / "features.csv",
     labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
     model_path: Path = MODELS_DIR / "model.pkl",
-    epochs: int = 20,
+    epochs: int = 50,
 ):
     # Load model and initial weights
-    model = None
-    weights = None
-    match model_version:
-        case 0:
-            weights = models.EfficientNet_B0_Weights.DEFAULT
-            model = models.efficientnet_b0(weights=weights)
-        case 1:
-            weights = models.EfficientNet_B1_Weights.IMAGENET1K_V1
-            model = models.efficientnet_b1(weights=weights)
-        case _:
-            logger.error(f"Model version {model_version} not recognized.")
-            return
-        
+    model, _ = get_model(model_version=model_version, use_weights=True)
+
     model.train()
 
     # Freeze all layers
-    for param in model.parameters():
+    for param in model.features.parameters():
         param.requires_grad = False
-    
+
     # Replace classifier
     # New classifier layer is unfrozen per default
-    model.classifier[1] = nn.Linear(
-        cast(nn.Linear, model.classifier[1]).in_features,
-        2
-    )
-    
+    model.classifier[1] = nn.Linear(cast(nn.Linear, model.classifier[1]).in_features, 2)
+
+    for param in model.classifier.parameters():
+        param.requires_grad = True
+
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    model.to(device)
+
     # Define loss function and optimizer
-    criterion = nn.CrossEntropyLoss(weight=None) # TODO: add class weights
-    optimizer = optim.Adam(
-        model.classifier.parameters()
-    )
+    criterion = nn.CrossEntropyLoss(weight=None)  # TODO: add class weights
+    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
 
     # Train model
     for epoch in range(epochs):
         model.train()
         total_loss = 0
 
-        for images, labels in tqdm(train_loader, desc=f"Epoch {epoch+1}/{epochs}"):
+        for images, labels in tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}"):
+            images, labels = images.to(device), labels.to(device)
+
             # Forward
             outputs = model(images)
             loss = criterion(outputs, labels)
@@ -77,7 +71,7 @@ def main(
             total_loss += loss.item()
 
         avg_loss = total_loss / len(train_loader)
-        
+
         # Evaluate on validation set
         model.eval()
 
@@ -85,22 +79,18 @@ def main(
         num_total = 0
         with torch.no_grad():
             for images, labels in validation_loader:
+                images, labels = images.to(device), labels.to(device)
+
                 outputs = model(images)
                 preds = outputs.argmax(dim=1)
 
-                num_correct += (
-                    preds == labels
-                ).sum().item()
+                num_correct += (preds == labels).sum().item()
 
                 num_total += labels.size(0)
 
         val_acc = num_correct / num_total
 
-        print(
-            f"Epoch {epoch+1}/{epochs} "
-            f"loss={avg_loss:.4f} "
-            f"val_acc={val_acc:.4f}"
-        )
+        print(f"Epoch {epoch + 1}/{epochs} loss={avg_loss:.4f} val_acc={val_acc:.4f}")
 
     # Save trained model
     torch.save(model.state_dict(), model_path)
